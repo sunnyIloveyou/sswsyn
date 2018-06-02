287482969e3a8d1d20ec0ca07671e4b7e7c92bad
xwp <xwp_fullstack@163.com>
2018-06-02 18:14:23 +0800

    目前告一段落 下期再见

--- info/modules/views.py
+++ info/modules/views.py
@@ -1,6 +1,14 @@
-from flask import session
+from flask import current_app
+from flask import session,render_template
+
 from . import ssw
 @ssw.route('/')
 def hello_world():
     session['name'] = 'python02'
-    return 'hello_world'
+    return render_template('news/detail.html')
+
+@ssw.route('/favicon.ico')
+def favicon():
+    # 返回小图标使用send_static_file()方法，要用应用上下文current——app调用
+    return current_app.send_static_file('news/favicon.ico')
+
