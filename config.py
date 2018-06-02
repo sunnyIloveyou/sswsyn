3af482ec3e86ae0feb2170b0006fffa85978c968
xwp <xwp_fullstack@163.com>
2018-06-02 15:18:38 +0800

    项目立项 哈哈哈wd

--- config.py
+++ config.py
@@ -1,2 +1,22 @@
+# StrictRedis 对象方法 用python与redis相互的时候都需要导入这个模块
+from redis import StrictRedis
+
 class Config(object):
     DEBUG = True
+    # 使用session 要设置密
+    SECRET_KEY = '7ovxKULnydmrdknfXFfOWRNWflMD5OuFzMdoz5S3uiA='
+    # 链接数据库
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost/syn'
+    #配置数据库动态追踪
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 配置ridis基本信息
+    SESSION_TYPE = 'redis'
+    # 对session信息是否加密
+    SESSION_USE_SIGNER = True
+    # redis对象
+    SESSION_REDIS = StrictRedis(host='localhost',port=6379)
+    #制定session过期时间以秒为单位
+    PERMANENT_SESSION_LIFETIME = 86400
+
+
+
--- manager.py
+++ manager.py
@@ -1,9 +1,27 @@
-from flask import Flask
+# 导入session
+from flask import Flask,session
+# 导入数据库
+from flask_sqlalchemy import SQLAlchemy
+# 导入管理器
+from flask_script import Manager
+# 导入迁移框架
+from flask_migrate import Migrate, MigrateCommand
+
+from flask_session import Session
 from config import Config
 app = Flask(__name__)
+db = SQLAlchemy(app)
+# 使用迁移框架
+Migrate(app,db)
+# 实例化管理器对象
+manager = Manager(app)
+# 绑定迁移命令
+manager.add_command('db',MigrateCommand)
 app.config.from_object(Config)
+Session(app)
 @app.route('/')
 def hello_world():
+    session['name'] = 'python02'
     return 'hello_world'
 if __name__ == '__main__':
-    app.run()
+    manager.run()
